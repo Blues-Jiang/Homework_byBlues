1ccc82e2734ac409b1f1b2082aa666d59c1fdc04
ACER-BLUES\Blues Jiang <victor1994y@qq.com>
2016-10-28 14:33:08 +0800

    finish basic function

--- Algorithm_by_Cpp/Algorithm_3/Algorithm_Homework_3_alpha.cpp
+++ Algorithm_by_Cpp/Algorithm_3/Algorithm_Homework_3_alpha.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #include <iomanip>
 #include <cmath>
+#include <vector>
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
@@ -40,16 +41,25 @@ public:
   QueenChess(int size);
   ~QueenChess();
   bool isSucceed();
+  bool findASolusion();
+  bool findNextSolusion();
+  void clearAll();
+
 
 };
 
 QueenChess::QueenChess(){
   size = 8;
   queenlist = new int[size+1];
+  queenlist[0] = 0;
+  for(int i=1;i<=size;i++)  queenlist[i] = 0;
 }
 
 QueenChess::QueenChess(int size){
   this->size = size;
+  queenlist = new int[size+1];
+  queenlist[0] = 0;
+  for(int i=1;i<=size;i++)  queenlist[i] = 0;
 
 }
 
@@ -58,7 +68,106 @@ QueenChess::~QueenChess(){
 }
 
 bool QueenChess::isSafe(int x,int y){
+  if( x>size || x<1 || y>size || y<1 )  return false;
+  for(int i=1;i<=size;i++){
+    if(i == x || queenlist[i] == 0) continue;
+    if(queenlist[i] == y) return false;
+    if((y-x) == (queenlist[i]-i)) return false;
+    if((y+x) == (queenlist[i]+i)) return false;
+  }
+  return true;
+}
 
+bool QueenChess::isSucceed(){
+  queenlist[0] = 0;
+  for(int i=1;i<=size;i++){
+    if(queenlist[i] != 0){
+      if(isSafe(i,queenlist[i]))  queenlist[0]++;
+      else                        return false;
+    }
+  }
+  return true;
+}
+
+
+void QueenChess::clearAll(){
+  for(int i=0;i<=size;i++){
+    queenlist[i] = 0;
+  }
+}
+
+bool QueenChess::findASolusion(){
+  int x,y;
+  do{
+    x=queenlist[0]+1;
+    y=queenlist[x]+1;
+    if(isSafe(x,y)){
+      queenlist[0]++;
+      queenlist[x]=y;
+    }
+    else if(y == size){
+      queenlist[0]--;
+      queenlist[x] = 0;
+    }
+    else{
+      queenlist[x]++;
+    }
+  }while(queenlist[0] > 0 || queenlist[0] < size);
+
+  if(queenlist[0] == 0) return false;
+  else                  return true;
+  }
+
+bool QueenChess::findNextSolusion(){
+  int x,y;
+  queenlist[queenlist[0]] = 0;
+  queenlist[0]--;
+  do{
+    x=queenlist[0]+1;
+    y=queenlist[x]+1;
+    if(isSafe(x,y)){
+      queenlist[0]++;
+      queenlist[x]=y;
+    }
+    else if(y == size){
+      queenlist[0]--;
+      queenlist[x] = 0;
+    }
+    else{
+      queenlist[x]++;
+    }
+  }while(queenlist[0] > 0 || queenlist[0] < size);
+
+  if(queenlist[0] == 0) return false;
+  else                  return true;
+}
+
+bool QueenChess::findFromRest(){
+    vector<int> vx;
+    int vxaddr=0;
+    int x,y;
+    for(int i=size;i>0;i--){
+      if(queenlist[i] == 0) vx.push_back(i);
+    }
+
+    do{
+      x=vx[vxaddr];
+      y=queenlist[x]+1;
+      if(isSafe(x,y)){
+        vxaddr++;
+        queenlist[x]=y;
+      }
+      else if(y == size){
+        vxaddr--;
+        queenlist[x] = 0;
+      }
+      else{
+        queenlist[x]++;
+      }
+    }while(vxaddr >= 0 || vxaddr < vx.size());
+
+    if(isSucceed()) return true;
+    else            return false;
 }
 
 int main(int argc, char *argv[])
