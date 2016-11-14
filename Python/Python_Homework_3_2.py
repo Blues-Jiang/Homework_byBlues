f2664ef644e207f8c151836162ed444350a80850
ACER-BLUES\Blues Jiang <victor1994y@qq.com>
2016-11-15 06:23:29 +0800

    need edit in Ubuntu

--- Python/Python_Homework_3_2.py
+++ Python/Python_Homework_3_2.py
@@ -3,18 +3,37 @@
 
 import re
 from graphics import *
+#from Tkinter import *
 
-
-def initialGUI():
+def GUI():
+    win = GraphWin("Statistic result by Blues",600,480)
+    win.setCoords(0.0,0.0,60.0,48.0)
 
 fl=open("list_pyHomework_3_2.txt",'r')
 buffer = fl.readline()
-sheetHeader = re.split(r'\s+',buffer)
+sheetHeader = re.split(r'\s+',buffer)   #Read the header of this sheet
 buffer = fl.readlines()
+
 sheetData = []
 for x in buffer:
     sheetData.append(re.split(r'\s+',x))
-subScoreList = {0:0,60:0,70:0,80:0,90:0,100:0}
-
-
 fl.close()
+
+subScoreList = {0:0,60:0,70:0,80:0,90:0,100:0,-1:0}
+scoreIndex = sheetHeader.index("成绩")
+for x in sheetData:
+    temp = int(x[scoreIndex])
+    if (temp < 60):
+        subScoreList[0] = subScoreList[0] + 1
+    else if (temp < 70):
+        subScoreList[60] = subScoreList[60] + 1
+    else if (temp < 80):
+        subScoreList[70] = subScoreList[70] + 1
+    else if (temp < 90):
+        subScoreList[80] = subScoreList[80] + 1
+    else if (temp < 100):
+        subScoreList[90] = subScoreList[90] + 1
+    else if (temp == 100):
+        subScoreList[100] = subScoreList[100] + 1
+    else
+        subScoreList[-1] = subScoreList[-1] + 1
