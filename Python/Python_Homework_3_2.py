6b4e0fe5cf6c6087ea7b263e3aa1062ef6087509
ACER-BLUES\Blues Jiang <victor1994y@qq.com>
2016-11-14 12:00:45 +0800

    update

--- Python/Python_Homework_3_2.py
+++ Python/Python_Homework_3_2.py
@@ -2,11 +2,19 @@
 # coding = utf-8
 
 import re
+from graphics import *
+
+
+def initialGUI():
 
 fl=open("list_pyHomework_3_2.txt",'r')
 buffer = fl.readline()
 sheetHeader = re.split(r'\s+',buffer)
 buffer = fl.readlines()
-sheetData = buffer
+sheetData = []
+for x in buffer:
+    sheetData.append(re.split(r'\s+',x))
+subScoreList = {0:0,60:0,70:0,80:0,90:0,100:0}
+
 
 fl.close()
